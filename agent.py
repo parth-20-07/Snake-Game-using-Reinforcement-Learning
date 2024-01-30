8df9ebab4132e3692465dcf38b5faf81f20f141b
Parth Patel <papatel@dekaresearch.com>
2024-01-30 16:48:22 -0500

    Setup Training Sequence

--- agent.py
+++ agent.py
@@ -11,7 +11,11 @@ LR = 0.001
 
 class Agent:
     def __init__(self) -> None:
-        pass
+        self.n_games = 0
+        self.epsilon = 0  # Parameter to control Randomness
+        self.gamma = 0  # Discount Rate
+        self.memory = deque(maxlen=MAX_MEM)  # Queue to store memory
+        # TODO:  Model , Trainer
 
     def get_state(self, game):
         pass
@@ -22,7 +26,7 @@ class Agent:
     def train_long_memory(self):
         pass
 
-    def train_short_memory(self):
+    def train_short_memory(self, state, action, rewards, next_state, done):
         pass
 
     def get_action(self, state):
@@ -30,7 +34,39 @@ class Agent:
 
 
 def train():
-    pass
+    plot_scores = []
+    plot_mean_score = []
+    total_score = 0
+    record = 0
+    agent = Agent()
+    game = SnakeGame()
+    while True:
+        # Get old states of the game
+        state_old = agent.get_state(game)
+
+        # Get Move
+        final_move = agent.get_action(state_old)
+
+        # Perform Move and get new State
+        reward, done, score = game.play_step(final_move)
+        state_new = agent.get_state(game)
+
+        # Train Short Memory
+        agent.train_short_memory(state_old, final_move, reward, state_new, done)
+
+        # Remember
+        agent.remember(state_old, final_move, reward, state_new, done)
+
+        if done:
+            # Train Long Memory
+            game.reset()
+            agent.n_games += 1
+            agent.train_long_memory()
+            if score > record:
+                record = score
+                # agent.model.save()
+
+            print(f"Game: {agent.n_games} | Score: {score} | Record: {record}")
 
 
 if __name__ == "__main__":
